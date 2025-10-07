a7ba13060e4fc349976ee44c447dda0c3f51f922
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-10-07 16:51:51 +0100

    Correct 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,5 +1,3 @@
-const firstLine = "C is fun"
-const secondLine = "Python is cool"
-const thirdLine = "JavaScript is amazing"
-
-console.log(firstLine, secondLine, thirdLine)
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
